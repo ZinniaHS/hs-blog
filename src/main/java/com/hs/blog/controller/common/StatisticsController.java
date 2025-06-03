eef3c0a933173d08b9f651bf7180472ebf4844a1
ZinniaHS <909623153@qq.com>
2025-06-03 21:55:39 +0800

    add StatisticsController

---
+++ src/main/java/com/hs/blog/controller/common/StatisticsController.java
@@ -0,0 +1,11 @@
+package com.hs.blog.controller.common;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "统计接口",description = "统计接口")
+@RequestMapping("/statistics")
+public class StatisticsController {
+
+}
